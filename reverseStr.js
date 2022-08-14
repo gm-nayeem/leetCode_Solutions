ee2ca2a39b2282188ad912f679a1c3687259aa43
golam mostafa <gmnayeem25@gmail.com>
2022-08-14 09:33:22 +0600

    reverse a string

---
+++ reverseStr.js
@@ -0,0 +1,15 @@
+let s = ["H","a","n","n","a","h"]
+
+let left = 0
+let right = s.length - 1
+
+while(left < right) {
+    let temp = s[left]
+    s[left] = s[right]
+    s[right] = temp
+
+    left++
+    right--
+}
+
+console.log(s);
